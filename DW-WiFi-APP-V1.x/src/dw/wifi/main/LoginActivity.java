05419c550769f8d9dc3941a660a3c04b0d0a55e7
winsonsunodm9n <gaozhenfa30972@126.com>
2021-06-28 11:28:07 +0800

    Signed-off-by: sean <1402330827@qq.com>

--- DW-WiFi-APP-V1.x/src/dw/wifi/main/LoginActivity.java
+++ DW-WiFi-APP-V1.x/src/dw/wifi/main/LoginActivity.java
@@ -8,8 +8,10 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class LoginActivity extends Activity {
 
@@ -17,6 +19,7 @@ public class LoginActivity extends Activity {
 	private Button btnWifiCfg;
 	private Button btnFastTest;
 	private Button btnTempTest;
+	private ImageButton imageFastBtn;
 	@SuppressLint("WorldReadableFiles")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +29,24 @@ public class LoginActivity extends Activity {
 		btnServerTest = (Button) findViewById(R.id.btnServerTest);
 		btnWifiCfg = (Button) findViewById(R.id.btnWifiCfg);
 		btnFastTest = (Button) findViewById(R.id.btnFastTest);	
-//		btnTempTest = (Button) findViewById(R.id.btnTempTest);
+		imageFastBtn = (ImageButton) findViewById(R.id.imageFastBtn);
 		
 		btnServerTest.setOnClickListener(new ButtonClickEvent());
 		btnWifiCfg.setOnClickListener(new ButtonClickEvent());
 		btnFastTest.setOnClickListener(new ButtonClickEvent());
-//		btnTempTest.setOnClickListener(new ButtonClickEvent());
+		imageFastBtn.setOnClickListener(new ButtonClickEvent());
+//		imageFastBtn.setOnTouchListener(new View.OnTouchListener(){            
+//		    public boolean onTouch(View v, MotionEvent event) {               
+//		            if(event.getAction() == MotionEvent.ACTION_DOWN){       
+//		               //重新设置按下时的背景图片  
+//		               ((ImageButton)v).setImageDrawable(getResources().getDrawable(R.drawable.android_btn_pressed));                              
+//		            }else if(event.getAction() == MotionEvent.ACTION_UP){       
+//		                //再修改为抬起时的正常图片  
+//		                ((ImageButton)v).setImageDrawable(getResources().getDrawable(R.drawable.android_btn));     
+//		            }  
+//		            return false;       
+//		    }       
+//		});
 	}
 	// ----------------------------------------------------清除按钮、发送按钮
 	class ButtonClickEvent implements View.OnClickListener {
@@ -49,10 +64,10 @@ public class LoginActivity extends Activity {
 				Intent intent = new Intent(LoginActivity.this,WIfiFastTestActivity.class);  
                 startActivity(intent);				
 			}
-//			else if(v == btnTempTest){
-//				Intent intent = new Intent(LoginActivity.this,TempTestActivity.class);  
-//                startActivity(intent);				
-//			}
+			else if(v == imageFastBtn){
+				Intent intent = new Intent(LoginActivity.this,WIfiFastTestActivity.class);  
+                startActivity(intent);				
+			}
 		}
 	}
 	@Override
